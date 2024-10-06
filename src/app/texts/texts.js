571b730c96bd917ea7f86ead95d9cc23063dc53c
kevinpicon01 <digitalpruu@gmail.com>
2024-10-05 20:05:43 -0500

    fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -16,7 +16,7 @@ const textos = {
             "Nuestro objetivo es ofrecer una experiencia única y deliciosa en cada sorbo, manteniendo el autentico y profundo sabor del cacao a través de recetas tradicionales y técnicas artesanales que nos permiten dar cuerpo y forma a la chucula, garantizando así que cada producto sea elaborado con la más alta calidad y mantenga las propiedades naturales de la chucula, con ese sabor inigualable que la hace única y original, lo que permite el deguste de paladares exquisitos y suculentos.\n" +
             "En la CHUCULERA además de la chucula, buscamos explorar nuevas formas de disfrutar el cacao, agregando ingredientes que realcen el sabor y aporten beneficios naturales a la salud del consumidor, por eso nos enorgullece ofrecer productos no solo deliciosos al paladar si no también saludables.\n" +
             "Descubre el secreto de los tres hervores con el sabor autentico de la chucula y del cacao, para que cada sorbo del chocolate de los siete granos lo transporte a un mundo de deleite y de placer.\n" +
-            "!EN LA CHUCULERA EL CACAO ES UNA EXPERIENCIA QUE VA MÁS ALLÁ DE LOS COMÚN!  "
+            "!EN LA CHUCULERA EL CACAO ES UNA EXPERIENCIA QUE VA MÁS ALLÁ DE LO COMÚN!  "
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
